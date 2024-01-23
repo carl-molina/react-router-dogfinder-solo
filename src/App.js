52df579e9e0e5c469a4c12037cb47f83e0bda9dd
Carl Molina <carlandrew.molina@gmail.com>
2024-01-22 22:01:52 -0800

    Replaced useEffect strat for fetch call to await fetch call instead, removed isGoodRequest state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 
 function App() {
+  const [dogs, setDogs] = useState({
+    data: null,
+    isLoading: true,
+  });
+
+  async function loadDogs() {
+    const resp = await fetch('http://localhost:5001/dogs');
+    const data = await resp.json();
+    setDogs({
+      data,
+      isLoading: false,
+    });
+  }
+
+  if (dogs.isLoading) {
+    loadDogs();
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+
     </div>
   );
 }
